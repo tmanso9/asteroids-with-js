0c2667c8f6693bc4cfdc9f9c5523a24003b125c8
tmanso9 <tmanso92@gmail.com>
2023-11-02 18:36:39 +0000

    feat: created player

--- index.js
+++ index.js
@@ -9,3 +9,33 @@ canvas.height = window.innerHeight
 ctx.fillStyle = 'black'
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
+class Player {
+	constructor({ position, velocity, color }) {
+		this.position = position
+		this.velocity = velocity
+		this.color = color
+	}
+
+	draw() {
+		// // Check center with a circle
+		// ctx.arc(this.position.x, this.position.y, 3, 0, Math.PI * 2)
+		// ctx.fillStyle = 'red'
+		// ctx.fill()
+
+		ctx.moveTo(this.position.x + 30, this.position.y)
+		ctx.lineTo(this.position.x - 10, this.position.y - 10)
+		ctx.lineTo(this.position.x - 10, this.position.y + 10)
+		ctx.closePath()
+
+		ctx.strokeStyle = this.color
+		ctx.stroke()
+	}
+}
+
+const player = new Player({
+	position: { x: canvas.width / 2, y: canvas.height / 2 },
+	velocity: { x: 0, y: 0 },
+	color: 'white'
+})
+
+player.draw()
